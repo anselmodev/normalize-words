8e66f31296a5016dfd8167de969c51f3f411b468
Anselmo Dev <anselmo.dev@gmail.com>
2020-10-16 17:08:32 -0300

    fix: fixed case sensitive error before removing character

--- src/utils/removeCharacters.util.ts
+++ src/utils/removeCharacters.util.ts
@@ -1,8 +1,10 @@
 const removeChar = (charactersToRemove: string[], charactersList: string[]) => {
-    let joinList: any = charactersList.join(' ');
+    let joinList: any = charactersList.join(' ').toLowerCase();
+
+    const charToRemove = charactersToRemove.map((item: string) => item.toLowerCase());
 
     for (let i = 0; i < charactersToRemove.length; i++) {
-        joinList = joinList.replace(new RegExp(charactersToRemove[i], "g"), '');
+        joinList = joinList.replace(new RegExp(charToRemove[i], "g"), '');
     }
 
     return joinList.split(' ');
